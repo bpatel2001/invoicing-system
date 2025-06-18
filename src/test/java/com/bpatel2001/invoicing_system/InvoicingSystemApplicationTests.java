e6ae907435b42e62fb051df11cee4fcc92fe4a16
bpatel2001 <bpatel2001h@gmail.com>
2025-06-18 14:31:16 -0500

    Initialized project

---
+++ src/test/java/com/bpatel2001/invoicing_system/InvoicingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bpatel2001.invoicing_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
