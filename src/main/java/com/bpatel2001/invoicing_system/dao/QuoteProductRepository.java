aab8856b8fa53abba48e22ed418e83336163ae16
bpatel2001 <bpatel2001h@gmail.com>
2025-06-19 11:35:48 -0500

    fixed jparepository extensions on quotes and quotesproducts daos

--- src/main/java/com/bpatel2001/invoicing_system/dao/QuoteProductRepository.java
+++ src/main/java/com/bpatel2001/invoicing_system/dao/QuoteProductRepository.java
@@ -1,9 +1,10 @@
 package com.bpatel2001.invoicing_system.dao;
 
-import com.bpatel2001.invoicing_system.entity.Quotes;
+import com.bpatel2001.invoicing_system.entity.QuotesProducts;
+import com.bpatel2001.invoicing_system.entity.QuotesProductsId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(path="quotesproducts")
-public interface QuoteProductRepository extends JpaRepository<Quotes, Long> {
+public interface QuoteProductRepository extends JpaRepository<QuotesProducts, QuotesProductsId> {
 }
--- src/main/java/com/bpatel2001/invoicing_system/dao/QuoteRepository.java
+++ src/main/java/com/bpatel2001/invoicing_system/dao/QuoteRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(path="quotes")
-public interface QuoteRepository extends JpaRepository<Quotes, Integer> {
+public interface QuoteRepository extends JpaRepository<Quotes, Long> {
 }
